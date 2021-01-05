8ab7e5f8ac2120f1a3d3f83687047cf046bbf333
七星 <oncwnuBTbwF0C2E02Ewqf7chmIfE@git.weixin.qq.com>
2021-01-05 18:15:11 +0800

    添加表单

--- react/react-tutorial/src/App.js
+++ react/react-tutorial/src/App.js
@@ -1,26 +1,10 @@
 import { Component } from "react";
 import Table from "./Table";
+import Form from './Form';
 
 class App extends Component {
     state = {
-        characters: [
-            {
-                'name': 'Charlie',
-                'job': 'Janitor'
-            },
-            {
-                'name': 'Mac',
-                'job': 'Bouncer'
-            },
-            {
-                'name': 'Dee',
-                'job': 'Aspring actress'
-            },
-            {
-                'name': 'Dennis',
-                'job': 'Bartender'
-            }
-        ]
+        characters: []
     };
 
     removeCharacter = (index) => {
@@ -33,6 +17,10 @@ class App extends Component {
         })
     }
 
+    handleSubmit = character => {
+        this.setState({characters: [...this.state.characters, character]});
+    }
+
     render() {
         const { characters } = this.state;
 
@@ -42,6 +30,7 @@ class App extends Component {
                     characterData={characters} 
                     removeCharacter={this.removeCharacter}
                 />
+                <Form handleSubmit={this.handleSubmit} />
             </div>
         );
     }
--- react/react-tutorial/src/Form.js
+++ react/react-tutorial/src/Form.js
@@ -4,12 +4,12 @@ class Form extends Component {
     constructor(props) {
         super(props);
 
-        this.initialStaate = {
+        this.initialState = {
             name: '',
             job: '',
         };
 
-        this.state = this.initialStaate;
+        this.state = this.initialState;
     }
 
     handleChange = event => {
@@ -19,4 +19,36 @@ class Form extends Component {
             [name] : value
         });
     }
+
+    submitForm = () => {
+        this.props.handleSubmit(this.state);
+        this.setState(this.initialState);
+    }
+
+    render() {
+        const { name, job } = this.state;
+    
+        return (
+            <form>
+                <label>Name</label>
+                <input
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={this.handleChange} />
+                <label>Job</label>
+                <input
+                    type="text"
+                    name="job"
+                    value={job}
+                    onChange={this.handleChange} />
+                <input 
+                    type="button"
+                    value="Submit"
+                    onClick={this.submitForm} />
+            </form>
+        );
+    }
 }
+
+export default Form;
