5eecc6e04a1c70d0f94d4f7c57374f4528fe0dee
七星 <oncwnuBTbwF0C2E02Ewqf7chmIfE@git.weixin.qq.com>
2021-01-07 00:02:35 +0800

    2021.01.06

--- react/react-guide/src/index.js
+++ react/react-guide/src/index.js
@@ -1,17 +1,140 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+/*
+class HelloWorld extends Component {
+    constructor(props) {
+        super(props)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        this.state = {
+            username: 'tylermcginnis'
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange (e) {
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                Hello {this.state.username} <br />
+                Change Name:
+                <input
+                    type="text"
+                    value={this.state.username}
+                    onChange={e => this.handleChange(e)}
+                />
+            </div>
+        );
+    }
+}
+
+const root = document.getElementById('root');
+
+ReactDOM.render(<HelloWorld />, root);
+*/
+
+/*
+class HelloUser extends Component {
+    render() {
+        return (
+            <div> Hello, {this.props.name}</div>
+        )
+    }
+}
+
+const root = document.getElementById('root');
+ReactDOM.render(<HelloUser name="Tyler" />, root);
+*/
+
+class FriendsContainer extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: 'Tyler McGinnis',
+            friends: [
+                'Jake Lingwall',
+                'Sarah Drasner',
+                'Merrick Christensen'
+            ],
+        }
+
+        this.addFriend = this.addFriend.bind(this)
+    }
+
+    addFriend(friend) {
+        this.setState((state) => ({
+            friends: state.friends.concat([friend])
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <h3> Name: {this.state.name}</h3>
+                <AddFriend addNew={this.addFriend} />
+                <ShowList names={this.state.friends} />
+            </div>
+        )
+    }
+}
+
+class ShowList extends Component {
+    render() {
+        return (
+            <div>
+                <h3> Friends </h3>
+                <ul>
+                    {this.props.names.map((friend) => {
+                        return <li>{friend}</li>
+                    })}
+                </ul>
+            </div>
+        )
+    }
+}
+
+class AddFriend extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            newFriend: ''
+        }
+
+        this.updateNewFriend = this.updateNewFriend.bind(this)
+        this.handleAddNew = this.handleAddNew.bind(this)
+    }
+    updateNewFriend(e) {
+        this.setState({
+            newFriend: e.target.value
+        })
+    }
+    handleAddNew() {
+        this.props.addNew(this.state.newFriend)
+        this.setState({
+            newFriend: ''
+        })
+    }
+    render() {
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={this.state.newFriend}
+                    onChange={this.updateNewFriend}
+                />
+                <button onClick={this.handleAddNew}> Add Friend </button>
+            </div>
+        )
+    }
+}
+
+const root = document.getElementById('root');
+ReactDOM.render(<FriendsContainer />, root);
